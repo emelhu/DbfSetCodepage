0c00acf9f8607f71ca0caecb80f328069a91d4b5
Laszlo Moravecz <emel@emel.hu>
2016-04-15 19:50:56 +0200

    DbfSetCodePageClass.cs added (It had copied)

---
+++ ___git_save_to_GitHub.bat
@@ -0,0 +1,7 @@
+﻿
+
+rem git remote add origin https://github.com/emelhu/DbfSetCodepage.git
+
+git push -u origin master
+
+pause
---
+++ create_link.bat
@@ -0,0 +1,9 @@
+rem mklink DbfSetCodePageClass.cs.link  ..\..\Commons\EMELCommonClassLibrary\dbf\DbfSetCodePageClass.cs
+rem mklink DbfSetCodePageClass          c:\eMeL_CSharp_Dev\Commons\EMELCommonClassLibrary\dbf\DbfSetCodePageClass.cs
+
+mklink /J   dbf   ..\..\Commons\EMELCommonClassLibrary\dbf
+
+rem  rmdir dbf
+
+pause
+
